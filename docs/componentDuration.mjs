07e040c18d21a0965cbc3a0d731dbe65bd32c0bc
Barbowza <barbowza@users.noreply.github.com>
2019-03-17 14:36:10 +0000

    Add duration modal Ok button and propogation of Ok via custom event to retrieve custom value and close modal

--- docs/componentDuration.mjs
+++ docs/componentDuration.mjs
@@ -4,35 +4,47 @@ const template = document.createElement('template');
 template.innerHTML = `
 <style>
 </style>
-<span id="outermost">
-    <select id="hours" aria-label="hours">
-        <option value="0">0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-    </select>
-    :
-    <select id="minutes" aria-label="minutes">
-        <option value="0">XX</option>
-    </select>
-    :
-    <select id="seconds" aria-label="seconds">
-        <option value="0">XX</option>
-    </select>
-</span>`;
+<div id="outermost">
+    <div>
+        <select id="hours" aria-label="hours">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+        :
+        <select id="minutes" aria-label="minutes">
+            <option value="0">XX</option>
+        </select>
+        :
+        <select id="seconds" aria-label="seconds">
+            <option value="0">XX</option>
+        </select>
+    </div>
+    <div>
+        <button id="ok">OK</button>
+    </div>
+</div>
+`;
+
 
 class ComponentDuration extends HTMLElement {
-    set value(value) {
-        this._value = value;
+    static get successEventName() {
+        return 'custom-duration';
+    } 
+
+    set duration(value) {
+        this._someValue = value;    // TODO
     }
 
     // The value of this component is the number of seconds duration selected
-    get value() {
+    get duration() {
         return (+this._hours * 60 * 60) + (+this._minutes * 60) + (+this._seconds);
     }
     
     constructor() {
         super();
+        this._debug = 0;
         this._hours = 0;
         this._minutes = 0;
         this._seconds = 0;
@@ -41,6 +53,7 @@ class ComponentDuration extends HTMLElement {
         this.shadowHours = this.populatedTemplate.getElementById('hours');
         this.shadowMinutes = this.populatedTemplate.getElementById('minutes');
         this.shadowSeconds = this.populatedTemplate.getElementById('seconds');
+        this.shadowOk = this.populatedTemplate.getElementById('ok');
         appendOptions(this.shadowHours, 12);
         appendOptions(this.shadowMinutes, 60);
         appendOptions(this.shadowSeconds, 60);
@@ -62,8 +75,16 @@ class ComponentDuration extends HTMLElement {
                     console.error('Unknown change target ', e.target);
                     break;
             }
-            console.log(`${this._hours} : ${this._minutes} : ${this._seconds}`)
-        })
+            this._debug && console.log(`${this._hours} : ${this._minutes} : ${this._seconds}`)
+        });
+
+        this.shadowOk.addEventListener('click', (e) => {
+            const event = new CustomEvent('custom-duration', { 
+                bubbles: true,
+                detail: {duration: this.duration} 
+            });
+            document.dispatchEvent(event);
+        });
     }
 
     // Fires every time this element connects to the DOM.
@@ -74,6 +95,10 @@ class ComponentDuration extends HTMLElement {
         }
     }
 }
+
+
+
+
   
 // Register our Component with the DOM
 const register = () => customElements.define(tagName, ComponentDuration);
@@ -96,3 +121,4 @@ function pad(num, size) {
     while (s.length < size) s = "0" + s;
     return s;
 }
+
