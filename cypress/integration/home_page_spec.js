2563dc909ce6592c911c500d04ec2d7bfd1c76c0
Barbowza <barbowza@users.noreply.github.com>
2019-04-01 19:23:13 +0100

    First cypress test

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,23 +1,24 @@
-describe('The Home Page', function() {
-    it('successfully loads', function() {
-      cy.visit('/docs/index.html'); // change URL to match your dev URL
-    })
-  })
+describe("The Home Page", function() {
+  it("successfully loads", function() {
+    cy.visit("/docs/index.html"); // change URL to match your dev URL
+  });
+});
 
-context('Controls', () => {
-    beforeEach(() => {
-        cy.viewport(411, 731);
-        cy.visit('/docs/index.html')
-    })
-  
-    describe('Start Pause', () => {
-      it('Start should start the timer', () => {
-          cy.get()
-      })
-  
-      it('.and() - chain multiple assertions together', () => {
-      })
-    })
-  
-  })
-  
+context("Controls", () => {
+  beforeEach(() => {
+    cy.viewport(411, 731);
+    cy.visit("/docs/index.html");
+  });
+
+  describe("Controls exist", () => {
+    it("has Start and Reset buttons", () => {
+      cy.get(".btn-start-pause").should("contain", "Start");
+      cy.get(".btn-reset").should("contain", "Reset");
+    });
+
+    it("has a Select", () => {
+      cy.get("#durationSelect").should("have.value", "5")
+    });
+
+  });
+});
