02a8473dcb993ebce1e0475d89bea6ae5fe72f48
Barbowza <barbowza@users.noreply.github.com>
2019-04-02 15:17:41 +0100

    Add data-js for main-pie-path. Test for Pie visible

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -18,5 +18,12 @@ context("Controls", () => {
       cy.get("#durationSelect").should("have.value", "5")
     });
 
+    it("has Main Pie visible and full", () => {
+      cy.get('[data-js="main-pie-path"]')
+        .should('be.visible')
+        .and('have.attr', 'd')
+        .and('include', 'M 1 0 A 1 1 0 1 1 1 -2.4492935982947064e-16 L 0 0')
+        ;
+    });
   });
 });
